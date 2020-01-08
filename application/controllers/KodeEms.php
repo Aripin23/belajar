15130eb81b27bbae5313ca17dd47fd7e532db544
Aripin <ilhamarifin.official@gmail.com>
2020-01-08 11:07:15 +0700

    add function ems

---
+++ application/controllers/KodeEms.php
@@ -0,0 +1,66 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class KodeEms extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('ems_model');
+    }
+
+    public function index()
+    {
+        $data['ems'] = $this->ems_model->get_Data();
+        $data['view'] = 'ems/index';
+        $this->load->view('template/master', $data);
+
+        // var_dump($data);
+        // die;
+    }
+
+
+    public function form_add()
+    {
+        $data['view'] = 'ems/add_ems';
+        $this->load->view('template/master', $data);
+    }
+    public function create()
+    {
+        $ems = $this->input->post('ems');
+        $data = array(
+            'kode_ems' => $ems
+        );
+
+        if($ems !=''){
+            $this->ems_model->insert_Data($data);
+        }
+        // var_dump('oke');
+        // die;
+        redirect('kodeEms');
+
+    }
+
+    public function form_update($id)
+    {
+        $data['ems'] = $this->ems_model->get_Data_id($id);
+        $data['view'] = 'ems/edit_ems';
+        $this->load->view('template/master', $data);
+    }
+
+    public function update($id)
+    {
+        $ems = $this->input->post('ems');
+        $data = array(
+            'kode_ems' => $ems
+        );
+        if($ems !=''){
+            $this->ems_model->update_Data($data, $id);
+        }
+        // var_dump($data);
+        // die;
+        redirect('kodeEms');
+    }
+
+}
---
+++ application/models/Ems_model.php
@@ -0,0 +1,28 @@
+<?php
+
+class Ems_model extends CI_Model {
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function get_Data(){
+        return $this->db->get('ems')->result();
+    }
+
+    public function get_Data_id($id)
+    {
+        return $this->db->get_where('ems',['id'=>$id])->row();
+    }
+
+    public function insert_Data($data)
+    {
+        $this->db->insert('ems', $data);
+    }
+
+    public function update_Data($data, $id)
+    {
+        $this->db->where('id', $id);
+        $this->db->update('ems', $data);
+    }
+
+}
