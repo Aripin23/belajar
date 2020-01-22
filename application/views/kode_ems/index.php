187b01d71a9ed621952e089d0fd7018716300b52
Aripin <ilhamarifin.official@gmail.com>
2020-01-22 11:43:45 +0700

    menambahkan icon pada tombol edit

--- application/views/kode_ems/index.php
+++ application/views/kode_ems/index.php
@@ -22,7 +22,7 @@
                             <td><?= $no++ ?></td>
                             <td><?= $d->kode_ems?></td>
                             <td>
-                            <?= '<a href=# onclick="editEms(\'' . $d->id . '\',\'' . $d->kode_ems . '\')" class="btn btn-warning" data-toggle="modal" data-target="#editEms">Edit</a>';
+                            <?= '<a href=# onclick="editEms(\'' . $d->id . '\',\'' . $d->kode_ems . '\')" class="btn btn-warning" data-toggle="modal" data-target="#editEms"><i class="fas fa-pencil-alt pr-2" aria-hidden="true"></i>Edit</a>';
                                 ?>
                             <a onclick="return confirm('Apakah data akan dihapus ?')" href="<?= base_url('KodeEms/delete/'.$d->id) ?>" type="button" class="btn btn-danger"><i class="fas fa-trash pr-2" aria-hidden="true"></i>
                             Hapus
