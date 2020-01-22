4b48af56ecefd64e3b957b428a2717da087b4408
Aripin <ilhamarifin.official@gmail.com>
2020-01-22 16:58:00 +0700

    menambahkan alert success bootstrap

--- application/controllers/Asosiasi.php
+++ application/controllers/Asosiasi.php
@@ -26,6 +26,9 @@ class Asosiasi extends CI_Controller
             'nama' => $nama
         ];
         $this->general_m->insertData('asosiasi', $data);
+        $this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan 
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span></button></div>');
         redirect('asosiasi');
     }
     public function edit()
