afe55a570f080b05799596a496a71c51ee106d31
Aripin <ilhamarifin.official@gmail.com>
2020-01-22 17:11:02 +0700

    menambahkan alert success bootstrap ref

--- application/controllers/KodeEms.php
+++ application/controllers/KodeEms.php
@@ -26,6 +26,9 @@ class KodeEms extends CI_Controller
             'kode_ems' => $kode_ems
         ];
         $this->general_m->insertData('ems', $data);
+        $this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan 
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span></button></div>');
         redirect('KodeEms');
     }
 
